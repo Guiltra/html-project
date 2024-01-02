b0a58d5cb170971fca9dab1da2a785ba296a9e16
Guiltra <140848590+Guiltra@users.noreply.github.com>
2024-01-02 18:32:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,93 +1,102 @@
-const inputs = document.querySelector(".word"),
-    hintTag = document.querySelector(".hint span"),
-    guessLeft = document.querySelector(".guess span"),
-    mistakes = document.querySelector(".wrong span"),
-    resetBtn = document.querySelector(".reset"),
-    hintBtn = document.querySelector(".showhint"),
-    hintElement = document.querySelector(".hint"),
-    typeInput = document.querySelector(".type-input");
+const playBoard = document.querySelector(".play-board");
+const scoreElement = document.querySelector(".score");
+const highScoreElement = document.querySelector(".high-score");
+const controls = document.querySelectorAll(".controls i");
 
-// Intializing game variables
-let word, incorrectLetters = [], correctLetters = [], maxGuesses;
+let gameOver = false;
+let foodX, foodY;
+let snakeX = 5, snakeY = 5;
+let velocityX = 0, velocityY = 0;
+let snakeBody = [];
+let setIntervalId;
+let score = 0;
 
-// Select random word from word list and set up game
-function startNewGame() {
-    alert("New Game Started! Guess New Word :)");
-    // Hide hint element
-    hintElement.style.display = "none";
-    hintElement.style.opacity = "0";
+// Get high score from local storage
 
-    // Choose random word from db and setup game
-    const ranWord = wordList[Math.floor(Math.random() * wordList.length)];
-    word = ranWord.word;
-    // If word chars >= 5 then max guess = 8 else max guess = 6
-    maxGuesses = word.length >= 5 ? 8 : 6;
-    incorrectLetters = [];
-    correctLetters = [];
-    hintTag.innerText = ranWord.hint;
-    guessLeft.innerText = maxGuesses;
-    mistakes.innerText = incorrectLetters;
+let highScore = localStorage.getItem("high-score") || 0;
+highScoreElement.innerText = `High Score: ${highScore}`;
 
-    // Create input for each letter of word
-    inputs.innerHTML = "";
-    for (let i = 0; i < word.length; i++) {
-        const input = document.createElement("input");
-        input.type = "text";
-        input.disabled = true;
-        inputs.appendChild(input);
+// Pass a random between 1 and 30 as food position
+
+const updateFoodPosition = () => {
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+}
+
+const handleGameOver = () => {
+    clearInterval(setIntervalId);
+    alert("Game Over! Press OK to replay...");
+    location.reload();
+}
+
+// Change velocity value based on key press
+
+const changeDirection = e => {
+    if (e.key === "ArrowUp" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+    } else if (e.key === "ArrowDown" && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;
+    } else if (e.key === "ArrowLeft" && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;
+    } else if (e.key === "ArrowRight" && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;
     }
 }
 
-// Handle user input and update game stats
-function handleInput(e) {
-    // Ignore non-letters input and letters that have already guessed
-    const key = e.target.value.toLowerCase();
-    if (key.match(/^[a-z]+$/i) && !incorrectLetters.includes(`${key}`) && !correctLetters.includes(`${key}`)) {
-        // Check if the letter is in word
-        if (word.includes(key)) {
-            // Update correct guess
-            for (let i = 0; i < word.length; i++) {
-                if (word[i] === key) {
-                    inputs.querySelectorAll("input")[i].value += key;
-                }
-            }
-            correctLetters += key;
-        } else {
-            // Update incorrect guess
-            maxGuesses--;
-            incorrectLetters.push(`${key}`);
-            mistakes.innerText = incorrectLetters;
+// Change Direction on each key click
+
+controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key })));
+
+const initGame = () => {
+    if (gameOver) return handleGameOver();
+    let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+
+    // When snake eat food
+    if (snakeX === foodX && snakeY === foodY) {
+        updateFoodPosition();
+        snakeBody.push([foodY, foodX]); //Add food to snake body array
+        score++;
+        highScore = score >= highScore ? score : highScore; // if score > high score => high score = score
+
+        localStorage.setItem("high-score", highScore);
+        scoreElement.innerText = `Score: ${score}`;
+        highScoreElement.innerText = `High Score: ${highScore}`;
+    }
+
+    // Update Snake Head
+    snakeX += velocityX;
+    snakeY += velocityY;
+
+    // Shifthing forward values of elements in snake body by one
+
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i - 1];
+    }
+
+    snakeBody[0] = [snakeX, snakeY];
+
+    // Check snake body is out of wall or no
+
+    if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+        return gameOver = true;
+    }
+
+    // Add div for each part of snake body
+
+    for (let i = 0; i < snakeBody.length; i++) {
+        html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+        // Check snake head hit body or no
+        if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
+            gameOver = true;
         }
     }
-
-    // Update remain guess and check for win lose conditions
-    guessLeft.innerText = maxGuesses;
-    if (correctLetters.length === word.length) {
-        alert(`Congrats! You Found The Word ${word.toUpperCase()}`);
-        startNewGame();
-    } else if (maxGuesses < 1) {
-        alert("Game Over! You Don't Have Remaining Guesses!");
-        for (let i = 0; i < word.length; i++) {
-            // Fill inputs with correct words
-            inputs.querySelectorAll("input")[i].value = word[i];
-        }
-    }
-
-    // Clear input field
-    typeInput.value = "";
+    playBoard.innerHTML = html;
 }
 
-// Show hint element
-function showHintElement() {
-    hintElement.style.display = "block";
-    hintElement.style.opacity = "1";
-}
-
-// Setup event listeners
-resetBtn.addEventListener("click", startNewGame);
-hintBtn.addEventListener("click", showHintElement);
-typeInput.addEventListener("input", handleInput);
-inputs.addEventListener("click", () => typeInput.focus());
-document.addEventListener("keydown", () => typeInput.focus());
-
-startNewGame();
+updateFoodPosition();
+setIntervalId = setInterval(initGame, 100);
+document.addEventListener("keyup", changeDirection);
